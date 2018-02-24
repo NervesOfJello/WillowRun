33a515724cef3329dcdb8225c78a6f7950bbec0b
Cole M. Bonnema <32077829+NervesOfJello@users.noreply.github.com>
2018-02-24 15:33:23 -0600

    Menus Finished

---
+++ Assets/Scripts/MenuButton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButton : MonoBehaviour {
+
+	public void toInstructions()
+    {
+        SceneManager.LoadScene("Instructions");
+    }
+
+    public void toInstructions2()
+    {
+        SceneManager.LoadScene("Instructions2");
+    }
+
+    public void toCredits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void toGame()
+    {
+        SceneManager.LoadScene("DemoScene");
+    }
+    public void toMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+}
